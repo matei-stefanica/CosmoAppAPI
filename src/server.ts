3c86949354f874b053f68e68b4e274e67c0db4d9
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-15 18:14:06 +0300

    refactored after the first review

--- src/server.ts
+++ src/server.ts
@@ -9,70 +9,72 @@ const app = express();
 const port = 8000;
 const axios = require('axios')
 
-const filePath : PathLike = 'cached-response.json';
+const FILEPATH : PathLike = 'cached-response.json';
 
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
+
+function sendResponseForNewImageOfTheDay(req : Request, res : Response) : void {
+    try {
+        const response = axios.get('https://api.nasa.gov/planetary/apod', {
+            params: {
+                api_key: apiKey
+            }
+        });
+        const stringRepresentationOfRespone : string | ArrayBufferView<ArrayBufferLike> = JSON.stringify(response.data, null);
+
+        fs.writeFile(FILEPATH, stringRepresentationOfRespone, (err) => {
+            if (err) {
+                console.error('Error writing file:', err);
+            } else {
+                console.log('File written successfully');
+            }
+        });
+        const urlToImage : string = response.data.url;
+        downloadImage(req, res, urlToImage);
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data from NASA');
+    }
+}
+
+function sendResponseForCachedImageOfTheDay(req : Request, res : Response) : void {
+    fs.readFile(FILEPATH, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error reading file:', err);
+        } else {
+            const cached_response = JSON.parse(data);
+            const urlToImage : string = cached_response.url;
+            downloadImage(req, res, urlToImage);
+            res.send(cached_response);
+
+        }
+    });
+}
+
+function downloadImage(req : Request, res : Response, urlToImage : string) {
+    const curlCommand : string = `curl -o "file.jpg" ` + urlToImage;
+
+    exec(curlCommand, (error : Error, stdout : string, stderr : Error) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return res.status(500).send('Error executing curl');
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
+    });
+}
 
 app.use(express.json());
 const router = express.Router();
 router.get('/cosmos', async(req : Request, res : Response) => {
-    if (!existsSync(filePath)) {
-        try {
-            const response = await axios.get('https://api.nasa.gov/planetary/apod', {
-                params: {
-                    api_key: "jk4uiZkVB9zdL0f1fhrUAeaBwdK8Bl3hydnL8umn"
-                }
-            });
-            const jsonString : string | ArrayBufferView<ArrayBufferLike> = JSON.stringify(response.data, null);
-
-            fs.writeFile('cached-response.json', jsonString, (err) => {
-                if (err) {
-                    console.error('Error writing file:', err);
-                } else {
-                    console.log('File written successfully');
-                }
-            });
-
-            const urlToImage : string = response.dat.url;
-            const curlCommand : string = `curl -o "file.jpg" ` + urlToImage;
-
-            exec(curlCommand, (error : Error, stdout : string, stderr : Error) => {
-                if (error) {
-                    console.error(`Error: ${error.message}`);
-                    return res.status(500).send('Error executing curl');
-                }
-                if (stderr) {
-                    console.error(`stderr: ${stderr}`);
-                }
-                res.send(response.data);
-
-            });
-
-        } catch (error) {
-            res.status(500).send('Error fetching data from NASA');
-        }
+    if (!existsSync(FILEPATH)) {
+        sendResponseForNewImageOfTheDay(req, res);
     }
     else {
-        fs.readFile('cached-response.json', 'utf8', (err, data) => {
-            if (err) {
-                console.log('Error reading file:', err);
-            } else {
-                const cached_response = JSON.parse(data);
-                const urlToImage : string = cached_response.url;
-                const curlCommand : string = `curl -o "file.jpg" ` + urlToImage;
-
-                exec(curlCommand, (error : Error, stdout : string, stderr : Error) => {
-                    if (error) {
-                        console.error(`Error: ${error.message}`);
-                        return res.status(500).send('Error executing curl');
-                    }
-                    if (stderr) {
-                        console.error(`stderr: ${stderr}`);
-                    }
-                    res.send(cached_response);
-                });
-            }
-        });
-        
+        sendResponseForCachedImageOfTheDay(req, res);
     }
     
 });
